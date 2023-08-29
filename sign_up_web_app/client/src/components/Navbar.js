59a2bee04b5db184fd8a66a1c5850f6933cd4522
Prasad W <88481022+MrPrasaD69@users.noreply.github.com>
2023-08-29 12:22:55 +0530

    Navbar toggle fixed.

--- sign_up_web_app/client/src/components/Navbar.js
+++ sign_up_web_app/client/src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 import { NavLink } from 'react-router-dom';
 function Navbar() {
   return (
@@ -7,7 +10,7 @@ function Navbar() {
         <nav className="navbar navbar-expand-lg bg-dark bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/dashboard">REACT APP</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation" >
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -15,11 +18,12 @@ function Navbar() {
                     <NavLink className="nav-link" to="/dashboard" activeclassname="active">DASHBOARD</NavLink>
                     <NavLink className="nav-link" to="/register" activeclassname="active">Register</NavLink>
                     <NavLink className="nav-link" to="/upload" activeclassname="active">Upload</NavLink>
-                    {/* <NavLink className="nav-link" to="#" activeClassName="active">Pricing</NavLink> */}
+                    
                 </div>
                 </div>
             </div>
         </nav>
+        
     </div>
   )
 }
